61c796987a0fe5b084dae03c26a579d566fee587
Emile Rolley <emile.rolley@tuta.io>
2024-11-07 15:50:49 +0100

    build: move the tsup config in a dedicated file and use terser for minification

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig((options) => {
+  return {
+    entry: [
+      "./src/index.ts",
+      "./publicodes-build/index.js",
+      "./src/data/index.ts",
+    ],
+    format: ["cjs", "esm"],
+    dts: true,
+    clean: true,
+    cjsInterop: true,
+    splitting: true,
+    minify: options.watch ? false : "terser",
+  };
+});
