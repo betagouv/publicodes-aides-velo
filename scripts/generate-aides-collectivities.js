153ee9f39ad29f9282578d436f7ba5f8d12763b6
Emile Rolley <emile.rolley@tuta.io>
2024-12-10 17:26:58 +0100

    fixme: should manage multiple localisations

--- scripts/generate-aides-collectivities.js
+++ scripts/generate-aides-collectivities.js
@@ -53,44 +53,51 @@ function extractCollectivityFromAST(rule) {
     "code insee",
   ];
 
-  const localisation = reduceAST(
+  const localisations = reduceAST(
     (acc, node) => {
-      if (acc) {
-        return acc;
-      }
       if (node.nodeKind === "operation" && node.operationKind === "=") {
         for (let kind of collectityKinds) {
           if (node.explanation[0]?.dottedName === `localisation . ${kind}`) {
-            return {
+            const loc = {
               kind,
               value: node.explanation[1]?.nodeValue,
             };
+            if (!acc.find((l) => l.kind === loc.kind && l.value == loc.value)) {
+              acc.push(loc);
+            }
+            return acc;
           }
         }
       }
     },
-    null,
+    [],
     rule
   );
 
-  if (!localisation) {
+  if (localisations.length === 0) {
     console.warn(`No localisation found for ${rule.dottedName}`);
     return;
   }
 
-  // In our rule basis we reference EPCI by their name but for iteroperability
-  // with third-party systems it is more robust to expose their SIREN code.
-  if (localisation.kind === "epci") {
-    const code = epci.find(({ nom }) => nom === localisation.value)?.code;
+  const normalizedLocalisations = localisations.map((loc) => {
+    // In our rule basis we reference EPCI by their name but for iteroperability
+    // with third-party systems it is more robust to expose their SIREN code.
+    if (loc.kind === "epci") {
+      const code = epci.find(({ nom }) => nom === loc.value)?.code;
 
-    if (!code) {
-      console.error(`Bad EPCI code: ${localisation.value}`);
-      exit(1);
+      if (!code) {
+        console.error(`Bad EPCI code: ${loc.value}`);
+        exit(1);
+      }
+
+      return { ...loc, code };
     }
 
-    return { ...localisation, code };
-  }
-  return localisation;
+    return loc;
+  });
+
+  // FIXME: should handle multiple localisations
+  return normalizedLocalisations[0];
 }
 
 // TODO: a bit fragile, we should sync this logic with
