92627b8a558442a28fe22e400bdc8c75c8826422
Clément <clem.auger@hotmail.fr>
2026-02-13 13:45:19 +0100

    feat: merge client and model script on model side

--- scripts/utils.js
+++ scripts/utils.js
@@ -13,3 +13,15 @@ export function getDataPath(filename) {
 
   return path.resolve(dataPath, filename);
 }
+
+/**
+ * Get the path to the dist data directory and create the directory if it doesn't exist.
+ */
+export function getDistDataPath(filename) {
+  const distDataPath = new URL("../dist/src/data", import.meta.url).pathname;
+  if (!fs.existsSync(distDataPath)) {
+    fs.mkdirSync(distDataPath, { recursive: true });
+  }
+
+  return path.resolve(distDataPath, filename);
+}
