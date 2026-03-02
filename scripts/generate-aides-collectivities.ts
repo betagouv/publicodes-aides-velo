616797774bdf890ce2ec19d2488af69c019ec067
Clément <clem.auger@hotmail.fr>
2026-03-02 14:33:55 +0100

    feat: type script

--- scripts/generate-aides-collectivities.ts
+++ scripts/generate-aides-collectivities.ts
@@ -4,7 +4,12 @@
 
 import fs from "node:fs";
 import { exit } from "process";
-import Publicodes, { reduceAST } from "publicodes";
+import Publicodes, {
+  ASTNode,
+  Evaluation,
+  reduceAST,
+  RuleNode,
+} from "publicodes";
 
 import epci from "@etalab/decoupage-administratif/data/epci.json" assert { type: "json" };
 import communes from "../src/data/communes.json" assert { type: "json" };
@@ -12,10 +17,43 @@ import departements from "@etalab/decoupage-administratif/data/departements.json
 import regions from "@etalab/decoupage-administratif/data/regions.json" assert { type: "json" };
 import { getDataPath, getDistDataPath } from "./utils.js";
 
-import rules from "../publicodes-build/index.js";
+import rules, { RuleName } from "../publicodes-build/index.js";
+
+type Commune = {
+  code: string;
+  nom: string;
+  departement: string;
+  region: string;
+  population: number;
+  zfe: boolean;
+  epci: string;
+  codesPostaux: string[];
+};
+
+type CollectivityKind =
+  | "pays"
+  | "région"
+  | "département"
+  | "epci"
+  | "code insee";
+
+type Collectivity = {
+  kind: CollectivityKind;
+  value: string;
+  code?: string;
+};
+
+interface AssociatedAideCollectivity {
+  collectivity: Collectivity;
+  codeInsee?: string;
+  region?: string;
+  departement?: string;
+  population?: number;
+  country: string;
+}
 
 const engine = new Publicodes(rules);
-const ruleNames = Object.keys(rules);
+const ruleNames = Object.keys(rules) as RuleName[];
 
 const rules_to_skip = [
   "aides . commune",
@@ -43,10 +81,12 @@ const aidesRuleNames = ruleNames.filter((ruleName) => {
   return false;
 });
 
-const communesSorted = communes.sort((a, b) => b.population - a.population);
+const communesSorted = (communes as Commune[]).sort(
+  (a, b) => b.population - a.population,
+);
 
-const extractCollectivityFromAST = (rule) => {
-  const localisationKinds = [
+const extractCollectivityFromAST = (rule: RuleNode): Collectivity => {
+  const localisationKinds: CollectivityKind[] = [
     "pays",
     "région",
     "département",
@@ -54,7 +94,7 @@ const extractCollectivityFromAST = (rule) => {
     "code insee",
   ];
 
-  const applicableSiNode = reduceAST(
+  const applicableSiNode = reduceAST<ASTNode | null>(
     (_, node) => {
       if (node.sourceMap?.mecanismName === "applicable si") {
         return node;
@@ -64,30 +104,56 @@ const extractCollectivityFromAST = (rule) => {
     rule,
   );
 
-  const { kind, value } = reduceAST(
+  if (!applicableSiNode) {
+    console.error(`No "applicable si" node found for rule ${rule.dottedName}`);
+    exit(1);
+  }
+
+  const localisationResult = reduceAST<
+    Set<{
+      kind: CollectivityKind;
+      value: string;
+    }>
+  >(
     (acc, node) => {
-      if (acc) return acc;
-      if (node.sourceMap?.mecanismName === "non applicable si") {
-        // skip non applicable si nodes
-        return acc;
-      }
       if (node.nodeKind === "operation" && node.operationKind === "=") {
         for (let localisationKind of localisationKinds) {
-          if (
-            node.explanation[0]?.dottedName ===
-            `localisation . ${localisationKind}`
-          ) {
-            return {
-              kind: localisationKind,
-              value: node.explanation[1]?.nodeValue,
-            };
+          const currentNodeDottedName = (
+            node.explanation[0] as ASTNode & { dottedName?: string }
+          )?.dottedName;
+          if (currentNodeDottedName === `localisation . ${localisationKind}`) {
+            const nodeValue = (
+              node.explanation[1] as ASTNode & { nodeValue: string }
+            )?.nodeValue;
+
+            if (!acc.has({ kind: localisationKind, value: nodeValue })) {
+              acc.add({ kind: localisationKind, value: nodeValue });
+            }
+            return acc;
           }
         }
       }
     },
-    null,
+    new Set(),
     applicableSiNode,
   );
+
+  if (localisationResult.size > 1) {
+    console.warn(
+      `Multiple localisations found in "applicable si" for rule ${rule.dottedName}, only the first one will be used.`,
+    );
+    console.log([...localisationResult]);
+  }
+
+  if (localisationResult.size === 0) {
+    console.error(
+      `No localisation found in "applicable si" for rule ${rule.dottedName}`,
+    );
+    exit(1);
+  }
+
+  // Should handle multiple localisations but in our current rule basis we only have one localisation per aide.
+  const { kind, value } = [...localisationResult][0];
 
   // In our rule basis we reference EPCI by their name but for interoperability
   // with third-party systems it is more robust to expose their SIREN code.
@@ -105,15 +171,17 @@ const extractCollectivityFromAST = (rule) => {
   return { kind, value };
 };
 
-const getCodeInseeForCollectivity = (collectivity) => {
+const getCodeInseeForCollectivity = (
+  collectivity: Collectivity,
+): string | undefined => {
   const { kind, value } = collectivity;
   switch (kind) {
     case "région":
       return regions.find(({ code: c }) => c === value)?.chefLieu;
     case "département":
       return departements.find(({ code: c }) => c === value)?.chefLieu;
+    // We use the most populated commune of the EPCI as the code insee for the EPCI.
     case "epci":
-      // value est le nom de l'EPCI, chercher une commune dans cet EPCI par le nom
       return communesSorted.find(({ epci }) => epci === value)?.code;
     case "code insee":
       return value;
@@ -122,19 +190,21 @@ const getCodeInseeForCollectivity = (collectivity) => {
   }
 };
 
-const getCommune = (codeInsee) =>
-  codeInsee && communesSorted.find(({ code }) => code === codeInsee);
+const getCommune = (codeInsee: string | undefined): Commune | undefined =>
+  codeInsee ? communesSorted.find(({ code }) => code === codeInsee) : undefined;
 
 // TODO: a bit fragile, we should sync this logic with
 // `engine.evaluate('localisation . pays')
-const getCountry = (rule) =>
+const getCountry = (rule: RuleNode) =>
   rule.dottedName === "aides . monaco"
     ? "monaco"
     : rule.dottedName === "aides . luxembourg"
     ? "luxembourg"
     : "france";
 
-const associateCollectivityMetadata = (rule) => {
+const associateCollectivityMetadata = (
+  rule: RuleNode,
+): AssociatedAideCollectivity => {
   const collectivity = extractCollectivityFromAST(rule);
   const codeInsee = getCodeInseeForCollectivity(collectivity);
   const commune = getCommune(codeInsee);
